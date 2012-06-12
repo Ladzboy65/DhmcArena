36dd83d081d3ab36c073a73be888882b9b40087f
Daniel Barnes <daniel-barnes@hotmail.com>
2012-06-12 15:31:56 -0600

    Added main class and started on arena joining

---
+++ src/me/Ladzboy65/DhmcArena.java
@@ -0,0 +1,69 @@
+package me.Ladzboy65;
+
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DhmcArena extends JavaPlugin{
+	
+	protected Logger log = Logger.getLogger("Minecraft");
+	
+	int x2 = getConfig().getInt("DhmcArena.getArena.x2");
+	int y2 = getConfig().getInt("DhmcArena.getArena.y2");
+	int z2 = getConfig().getInt("DhmcArena.getArena.z2");
+	int x1 = getConfig().getInt("DhmcArena.getArena.x1");
+	int y1 = getConfig().getInt("DhmcArena.getArena.y1");
+	int z1 = getConfig().getInt("DhmcArena.getArena.z1");
+	
+	
+public void onEnable(){
+		
+		this.log("This plugin has been enabled!");
+}
+
+public void onDisable(){
+	
+	this.log("This plugin has been disabled!");
+}
+
+public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+	Player player = (Player) sender;
+	if(cmd.getName().equalsIgnoreCase("ma")){
+		if(args[0].equalsIgnoreCase("join") );{
+ 
+			InArena ia = new InArena();
+			ia.JoinArena(player);
+	} if (args[0].equalsIgnoreCase("Vive") );{
+		}if(args[0].equalsIgnoreCase("Vive") );{
+}
+
+	}
+	return false;
+}
+
+		/**
+		 * 
+		 * @param message
+		 */
+		public void log(String message){
+			log.info("[dhmcArena]: " + message);
+		}
+
+		public void saveInventory(Player player) {
+			Inventory inv = player.getInventory();
+			player.getInventory().clear();
+			player.updateInventory();
+		}
+		public void giveClassInventory(Player player){
+			
+			
+		}
+		public void giveBackInventory(Player player){
+			
+			
+		}
+}
